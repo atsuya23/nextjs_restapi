1b5fe79b8815156b56d1cf9a03c7671dd2ab1bea
90808047+atsuya23@users.noreply.github.com <90808047+atsuya23@users.noreply.github.com>
2021-10-01 10:13:13 +0900

    for deploy(settings)

--- nextjs_restapi/settings.py
+++ nextjs_restapi/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ['nextjs-api-by-django.herokuapp.com']
+ALLOWED_HOSTS = ['nextjs-restapi-by-django.herokuapp.com/']
 
 # Application definition
 
